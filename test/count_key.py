057761c30089e5b82ff75a0e6589363d01417d96
Jarvis_N <non.ny1999@hotmail.com>
2025-08-07 22:05:24 +0700

    Update count_key.py

--- test/count_key.py
+++ test/count_key.py
@@ -1,23 +1,23 @@
 import keyboard
 
-# count = 0
-# is_pressed = False
+count = 0
+is_pressed = False
 
-# print("กด Spacebar เพื่อเริ่มนับจำนวนครั้งที่กดและปล่อย (กด Esc เพื่อออก)")
+print("กด Spacebar เพื่อเริ่มนับจำนวนครั้งที่กดและปล่อย (กด Esc เพื่อออก)")
 
-# while True:
-#     # ถ้า space ถูกกดลง
-#     if keyboard.is_pressed("space") and not is_pressed:
-#         is_pressed = True
+while True:
+    # ถ้า space ถูกกดลง
+    if keyboard.is_pressed("space") and not is_pressed:
+        is_pressed = True
 
-#     # ถ้า space ถูกปล่อย
-#     if not keyboard.is_pressed("space") and is_pressed:
-#         count += 1
-#         print(f"ปล่อย Spacebar ครั้งที่ {count}")
-#         is_pressed = False
+    # ถ้า space ถูกปล่อย
+    if not keyboard.is_pressed("space") and is_pressed:
+        count += 1
+        print(f"ปล่อย Spacebar ครั้งที่ {count}")
+        is_pressed = False
 
-#     # ออกจากโปรแกรมเมื่อกด Esc
-#     if keyboard.is_pressed("esc"):
-#         print("ออกจากโปรแกรม")
-#         break
+    # ออกจากโปรแกรมเมื่อกด Esc
+    if keyboard.is_pressed("esc"):
+        print("ออกจากโปรแกรม")
+        break
 
